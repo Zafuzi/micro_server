1c92398d14ee42e58f119e59336618c77bd1dd87
Zafuzi <zacharyfoutz@gmail.com>
2021-04-26 09:17:36 -0700

    * cleaned up micro ping function

--- micro.js
+++ micro.js
@@ -22,24 +22,5 @@ module.exports = class Micro {
 		L.D( this.data?.msg || "no message" );
 		this.okay();
 	}
-	ping() {
-		var self = this;
-		const https = require('https');
-		https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-		  let data = '';
-
-		  // A chunk of data has been received.
-		  resp.on('data', (chunk) => {
-		    data += chunk;
-		  });
-
-		  // The whole response has been received. Print out the result.
-		  resp.on('end', () => {
-		    self.okay(data.explanation);
-		  });
-
-		}).on("error", (err) => {
-			self.fail("Error: " + err.message);
-		});
-	}
+	ping() { this.okay("okay"); }
 }
