794c1e29606f443b0e0dca650bed8d0c6087266b
Joe Hitchens <joe@sleepless.com>
2021-04-26 18:33:37 -0700

    changes by joe

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ _______________________________________________________________________________
   
                                 MICRO SERVER
 
-                            node, express, cors
+                            node, connect, cors
 
 _______________________________________________________________________________
 
@@ -12,18 +12,17 @@ _______________________________________________________________________________
 
 - about -
 
-Micro Server is a simple template for starting a new project that requires a api and a simple frontend
+Micro Server is a simple template for starting a new project that requires an api and a simple frontend
 
-add functions to the Micro class in micro.js to add functions to the api
-and edit frontend/local.js to see how to call the api with a simple POST
+Add code to "api/index.js" to handle api calls however you like.
 
-
-
+Static website files go in "static/"
+See "static/local.js" to see how to call the api with a simple POST
 
 - structure -
 
 	index.js : router
-	micro.js : api
-	frontend/
+	api/index.js : api
+	static/ :
 		index.html
 		local.js
--- index.js
+++ index.js
@@ -2,58 +2,18 @@
 delete require.cache[module.filename]; // always reload
 const HERE = require("path").dirname( module.filename );
 
-const	path			= require("path"),
-		fs				= require("fs"),
-		connect			= require("connect"),
-		serve_static 	= require( "serve-static" ),
-		L				= require("log5").mkLog("\tmicro_server: ")(5);
-
 require( "sleepless" ).globalize();
-
-
-// RPC Controller
-const rpc = function( req, res, next ) {
-
-	const done = ( error, data ) => {
-		if(error) L.E(o2j(error));
-		let json = JSON.stringify( { error, data } );
-		res.writeHead( 200, {
-			"Cache-Control": "max-age=0, no-store",
-			"Content-Type": "application/json",
-		});
-		res.write( json );
-		res.end();
-		next();
-	};
-	const fail = ( error, data ) => { done( error, data ); };
-	const okay = ( data ) => { done( null, data ); };
-
-	let body 	= req.method == "GET" ? req.query : req.body;
-
-	let cmd 	= body.cmd;
-	if( ! cmd ) {
-		fail( "No command given" );
-		return;
-	}
-
-	const Micro = require("./micro.js");
-	let m = new Micro( body, okay, fail );
-	if( ! m[cmd] ) {
-		fail( `Command does not exist: ${cmd}` );
-		return;
-	}
-
-	L.D( "got here" );
-
-	m[cmd]();
-}
+const	connect			= require("connect"),
+		serve_static 	= require( "serve-static" ),
+		L = log5.mkLog( "\tmicro_server: " )(5);
 
 const app = connect();
 
 app.use( require( "body-parser" ).json() );
 app.use( require( "cookie-parser" )() );
 app.use( require( "compression" )() );
-//app.use( require( "cors" )() );
+//app.use( require( "cors" )() );	// enable to handle requests from other domains
+app.use( mw_fin_json );		// adds okay, fail to res (defined in sleepless module)
 app.use( ( req, res, next ) => {
 	req.query = require('querystring').parse( req._parsedUrl.query );
 	next();
@@ -61,8 +21,18 @@ app.use( ( req, res, next ) => {
 
 // API path handler
 app.use( ( req, res, next ) => {
-	if( /GET|POST/.test( req.method ) && req.url.startsWith( "/api/" ) ) {
-		rpc( req, res, next );
+	const { method, url, query, body } = req;
+	L.V( method + " " + url );
+	if( /GET|POST/.test( method ) && url.startsWith( "/api/" ) ) {
+		try {
+			const path = HERE + "/api";
+			const mod = require( path );
+			const input = ( method == "GET" ) ? query : body;
+			mod( input, res.okay, res.fail );	
+		} catch( e ) {
+			L.E( e.stack );
+			next();
+		}
 		return;
 	}
 	next();
