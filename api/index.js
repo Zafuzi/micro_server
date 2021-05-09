cc53c4234251a289706590f18ffe60b48f6066d8
zafuzi <zacharyfoutz@gmail.com>
2021-05-08 20:06:12 -0700

    made changes to work with digitalocean apps

--- api/index.js
+++ api/index.js
@@ -4,6 +4,11 @@ const HERE = require("path").dirname( module.filename );
 
 require( "sleepless" ).globalize();
 
+console.log("h " + process.env.HOSTNAME );
+console.log("u " + process.env.USERNAME );
+console.log("p " + process.env.PASSWORD );
+console.log("d " + process.env.DATABASE );
+
 module.exports = function( input, okay, fail ) {
 
 	const { cmd, msg } = input;
--- server.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const PORT = process.argv[2] || 12345;
-http.createServer(require('./index.js')).listen(PORT, () => {
-    console.log( `\n\n--------------------Listening on port ${PORT}--------------------\n\n`);
-});
