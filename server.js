c0fef044dbb9a3fde71fab8d4f6d9c30cd85eed9
zafuzi <zacharyfoutz@gmail.com>
2021-05-08 19:42:23 -0700

    package.json

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
-http.createServer(require('./index.js')).listen(12345, () => {
-    console.log( "\n\n--------------------Listening on port 12345--------------------\n\n");
+const PORT = process.argv[2] || 12345;
+http.createServer(require('./index.js')).listen(PORT, () => {
+    console.log( `\n\n--------------------Listening on port ${PORT}--------------------\n\n`);
 });
